1bff1b3e188a300d9126139ba29b9951c0d3e09d
green1052 <cydyellowgreen@gmail.com>
2020-12-14 17:11:19 +0900

    Update MunpiaMaterialDarkTheme.user.js

--- MunpiaMaterialDarkTheme.user.js
+++ MunpiaMaterialDarkTheme.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name		MunpiaMaterialDarkTheme
-// @version		1.0.1
+// @version		1.0.2
 // @author		green1052
 // @description	문피아 글 배경을 어둡게 만들어 줍니다.
 // @homepageURL	https://github.com/green1052/MunpiaMaterialDarkTheme
 // @downloadURL	https://raw.githubusercontent.com/green1052/MunpiaMaterialDarkTheme/main/MunpiaMaterialDarkTheme.user.js
-// @include		*.munpia.com/*/*
+// @include		*.munpia.com/*
 // @run-at		document-end
 // ==/UserScript==
 
